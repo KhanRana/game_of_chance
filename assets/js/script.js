c2d56c6e495f80a56845de81c2e80309a31f5f1c
KhanRana <rana_khan@outlook.com>
2022-10-13 06:17:40 +0000

    adjust image resolutions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(){
     let buttons = document.getElementsByTagName("button");
 })
+
+
+
+let image = document.getElementById("your-image");
+if (image.src.match("assets/images/RockPaperScissorsLizardSpock.jpg")) {
+    // image.src = "assets/images/rock.jpeg";
+}
