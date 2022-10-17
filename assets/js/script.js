272236c81ef94fc3c5274e97f828c4ec27fe339d
KhanRana <rana_khan@outlook.com>
2022-10-17 23:05:19 +0000

    use nested loops for answer check and style reset

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,85 +1,118 @@
 const choices = ["rock", "paper", "scissors", "lizard", "spock"];
 
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
 
     let buttons = document.getElementsByTagName("button");
     for (let button of buttons) {
-        button.addEventListener("click", function(){
+        button.addEventListener("click", function () {
             let btn = this.getAttribute("data-type");
-            choice(btn, "your-image")[0]; //change player image
+
+            let playerImage;
+            playerImage = choice(btn, "your-image")[0];
 
             let yourChoice = choice(btn, "your-image")[1]; // get player choice
-         
+
             let ncp = npcChoice(choices); //npc choice from the array
-            choice(ncp, "npc-image")[0]; // get npc image
+            let myImage;
+            myImage = choice(ncp, "npc-image")[0]; // get npc image
 
             let myChoice = choice(ncp, "npc-image")[1]; //get npc choice 
+            console.log(myChoice);
+            checkAnswer(yourChoice, myChoice); //check choices and winners 
 
-            checkAnswer(yourChoice, myChoice);//check choices and winners 
-            
             winner();
-        })  
+        })
     }
 
-    resetScores();  //reset the score and winner
+    resetScores(); //reset the score and winner
 })
 
 /**
  * make a choice and returns image and choice
  */
-function choice(btn, id) { 
+function choice(btn, id) {
     let img = document.getElementById(id);
-    if (btn === "rock"){
-    return [img.src = "assets/images/rock.jpeg", "rock"];
-}
-    else if (btn === "paper"){
-     return [img.src = "assets/images/paper.jpeg", "paper"];
-}
-    else if (btn === "scissors"){
-    return [img.src = "assets/images/scissors.jpeg", "scissors"];
-}
-else if (btn === "lizard"){
-    return [img.src = "assets/images/lizard.jpeg", "lizard"];
-}
-else if (btn === "spock"){
-    return [img.src = "assets/images/spock.jpeg", "spock"];
-}       
+    if (btn === "rock") {
+        return [img.src = "assets/images/rock.jpeg", "rock"];
+    } else if (btn === "paper") {
+        return [img.src = "assets/images/paper.jpeg", "paper"];
+    } else if (btn === "scissors") {
+        return [img.src = "assets/images/scissors.jpeg", "scissors"];
+    } else if (btn === "lizard") {
+        return [img.src = "assets/images/lizard.jpeg", "lizard"];
+    } else if (btn === "spock") {
+        return [img.src = "assets/images/spock.jpeg", "spock"];
+    }
 }
 
 /**
  * function to get npc choice
  */
-function npcChoice(choice){
+function npcChoice(choice) {
     return choice[Math.floor(Math.random() * choices.length)];
 }
 
 /**
  * check choices and display who wins the round
  */
-function checkAnswer(playerChoice, nonPlayer){
+function checkAnswer(playerChoice, nonPlayer) {
     // check for a tie round
     let message = ``;
     if (nonPlayer === playerChoice) {
         message = `We both chose ${nonPlayer} - Its a tie`;
         document.getElementById("round-winner").textContent = message;
-    }
-    else if ((nonPlayer === "rock") && (playerChoice === "scissors" || playerChoice === "lizard")
-    || ((nonPlayer === "paper") && (playerChoice === "rock" || playerChoice === "spock")) 
-    || ((nonPlayer === "scissors") && (playerChoice === "paper" || playerChoice === "lizard"))
-    || ((nonPlayer === "lizrad") && (playerChoice === "paper") || playerChoice === "spock")
-    || ((nonPlayer === "spock") && (playerChoice === "rock" || playerChoice === "scissors"))) {
+        
+    } else if (nonPlayer === "rock") {
+        if (playerChoice === "scissors" || playerChoice === "lizard") {
+            message = `I chose ${nonPlayer}, so I win! ${nonPlayer} beats ${playerChoice}`;
+            incrementNPCScore();
 
-        message = `I chose ${nonPlayer}, so I win! ${nonPlayer} beats ${playerChoice}`;
-        document.getElementById("round-winner").textContent = message;
-        incrementNPCScore();
-    }
-    else {
-        message = `I chose ${nonPlayer}, so you win! ${playerChoice} beats ${nonPlayer}`;
-        document.getElementById("round-winner").textContent = message;
-        incrementScore();
-    }
+        } else {
+            message = `I chose ${nonPlayer}, so you win! ${playerChoice} beats ${nonPlayer}`;
+            incrementScore();
+        }
+    } else if (nonPlayer === "paper") {
+        if (playerChoice === "rock" || playerChoice === "spock") {
+            message = `I chose ${nonPlayer}, so I win! ${nonPlayer} beats ${playerChoice}`;
+            incrementNPCScore();
 
+        } else {
+            message = `I chose ${nonPlayer}, so you win! ${playerChoice} beats ${nonPlayer}`;
+            incrementScore();
 
+        }
+    } else if (nonPlayer === "scissors") {
+        if (playerChoice === "paper" || playerChoice === "lizard") {
+            message = `I chose ${nonPlayer}, so I win! ${nonPlayer} beats ${playerChoice}`;
+            incrementNPCScore();
+
+        } else {
+            message = `I chose ${nonPlayer}, so you win! ${playerChoice} beats ${nonPlayer}`;
+            incrementScore();
+
+        }
+    } else if (nonPlayer === "lizard") {
+        if (playerChoice === "paper" || playerChoice === "spock") {
+            message = `I chose ${nonPlayer}, so I win! ${nonPlayer} beats ${playerChoice}`;
+            incrementNPCScore();
+
+        } else {
+            message = `I chose ${nonPlayer}, so you win! ${playerChoice} beats ${nonPlayer}`;
+            incrementScore();
+
+        }
+    } else if (nonPlayer === "spock") {
+        if (playerChoice === "rock" || playerChoice === "scissors") {
+            message = `I chose ${nonPlayer}, so I win! ${nonPlayer} beats ${playerChoice}`;
+            incrementNPCScore();
+
+        } else {
+            message = `I chose ${nonPlayer}, so you win! ${playerChoice} beats ${nonPlayer}`;
+            incrementScore();
+
+        }
+    }
+    document.getElementById("round-winner").textContent = message;
 
 }
 
@@ -103,17 +136,14 @@ function incrementNPCScore() {
 /**
  * function to track the winner
  */
-function winner (){
-    if (parseInt(document.getElementById("your-score").innerText) > 
-    parseInt(document.getElementById("npc-score").innerText))
-    {
+function winner() {
+    if (parseInt(document.getElementById("your-score").innerText) >
+        parseInt(document.getElementById("npc-score").innerText)) {
         document.getElementById("winner").textContent = "You are winning the Game!";
-    }
-    else if (parseInt(document.getElementById("npc-score").innerText) > 
-    parseInt(document.getElementById("your-score").innerText)){
+    } else if (parseInt(document.getElementById("npc-score").innerText) >
+        parseInt(document.getElementById("your-score").innerText)) {
         document.getElementById("winner").textContent = "I am winning the Game!";
-    }
-    else {
+    } else {
         document.getElementById("winner").textContent = "Its draw!";
     }
 }
@@ -121,12 +151,15 @@ function winner (){
 /**
  * reset the score
  */
-function resetScores(){
+function resetScores() {
     let reset = document.getElementById("reset");
-    reset.addEventListener("click", function (){
+    reset.addEventListener("click", function () {
         document.getElementById("your-score").textContent = 0;
         document.getElementById("npc-score").textContent = 0;
         document.getElementById("round-winner").textContent = '';
         document.getElementById("winner").textContent = '';
+        document.getElementById("your-image").src="assets/images/RockPaperScissorsLizardSpock.jpg";
+        document.getElementById("npc-image").src="assets/images/RockPaperScissorsLizardSpock.jpg"; 
+ 
     })
 }
